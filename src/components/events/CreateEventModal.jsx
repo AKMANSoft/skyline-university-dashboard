1bd875aaa8621c77813af6c61cc3441111e96a29
Syed Aqeel Haider <haideraqeel86@hotmail.com>
2024-05-07 20:47:07 +0500

    event modal modified

--- src/components/events/CreateEventModal.jsx
+++ src/components/events/CreateEventModal.jsx
@@ -10,7 +10,7 @@ const style = {
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
-    width: 600,
+    width: {xs:320,sm:600},
     bgcolor: "background.paper",
     boxShadow: 24,
     borderColor: "0px solid transparent",
